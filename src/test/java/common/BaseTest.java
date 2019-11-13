1501a925203cbfba3c5c6254da2dc70a33371101
dodarchenko <dmitriy.odarchenko@gmail.com>
2019-11-13 17:30:49 +0200

    Add parameters to some capabilities

--- src/test/java/common/BaseTest.java
+++ src/test/java/common/BaseTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -15,16 +17,15 @@ public class BaseTest {
     public AppiumDriver<WebElement> driver;
 
     @BeforeSuite(alwaysRun = true)
-    public void setUp() throws MalformedURLException {
+    @Parameters({"deviceID", "osVersion", "deviceName"})
+    public void setUp(String deviceID, String osVersion, String deviceName) throws MalformedURLException {
 
         //Set the Desired Capabilities
         DesiredCapabilities caps = new DesiredCapabilities();
-        //caps.setCapability("deviceName", "Pixel 2");
-        //caps.setCapability("udid", "emulator-5554");
-        caps.setCapability("deviceName", "NEXUS 5X");
-        caps.setCapability("udid", "025ab90b307ecdd0"); //Give Device ID of your mobile phone
+        caps.setCapability("deviceName", deviceName);
+        caps.setCapability("udid", deviceID); //Give Device ID of your mobile phone
         caps.setCapability("platformName", "Android");
-        caps.setCapability("platformVersion", "8.0");
+        caps.setCapability("platformVersion", osVersion);
         caps.setCapability("appPackage", "com.dev.ct.dev");
         caps.setCapability("appActivity", "mobile.droid.ctrader.views.main.SplashScreen");
         caps.setCapability("noReset", "true");
@@ -32,7 +33,6 @@ public class BaseTest {
 
         //Instantiate Appium Driver
 
-        //driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
         driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
         driver.resetApp();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
